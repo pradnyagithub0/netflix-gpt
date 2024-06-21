30a9bbbdd9ef4b103984041ac002a1ced05f36c5
Pradnya <mailto:waghmarepradnya0611@gmail.com>
2024-06-21 15:28:33 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className='flex justify-center text-red-600 text-3xl font-bold'>Hello Pradnya </h2>
+    </div>
+  );
+}
+
+export default App;
